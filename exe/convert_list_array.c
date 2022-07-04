665b30ff01f6db9de2cbb9d30e7c048cdd12d307
Marius Kölle <marius@Mariuss-MBP.fritz.box>
2022-07-04 20:00:01 +0200

    added: pipes now working. buggy: redirections

--- exe/convert_list_array.c
+++ exe/convert_list_array.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   convert_list_array.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmichael <nmichael@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marius <marius@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:48:54 by nmichael          #+#    #+#             */
-/*   Updated: 2022/06/03 13:58:11 by nmichael         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:58:03 by marius           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	executer_b(t_exe_locals *locals)
 {
-	safe_fd_set(&locals->fd_in, STDIN_FILENO);
-	safe_fd_set(&locals->fd_out, STDOUT_FILENO);
-	safe_fd_set(&locals->fd_next, STDIN_FILENO);
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	dup2(locals->fd_standard_out, STDOUT_FILENO);
+	dup2(locals->fd_standard_in, STDIN_FILENO);
 	locals->i = 0;
 	while (locals->i < locals->j)
 	{
@@ -28,20 +29,16 @@ void	executer_b(t_exe_locals *locals)
 int	executer(t_input *input, t_env2 *env2)
 {
 	t_exe_locals	locals;	
+	int	ret;
 
 	g_state[0] = EXEC;
 	init_locals(&locals);
 	locals.pa = process_count(input, &locals.j);
+	ret = 0;
+	close (STDIN_FILENO);
+	dup2(locals.fd_standard_in, STDIN_FILENO);
 	while (locals.i < locals.j)
 	{
-		safe_fd_set(&locals.fd_in, locals.fd_next);
-		if (locals.i == locals.j - 1)
-			safe_fd_set(&locals.fd_out, STDOUT_FILENO);
-		else
-		{
-			if (!safe_pipe(&locals.fd_out, &locals.fd_next))
-				return (2);
-		}
 		check_for_dollar(input, env2);
 		locals.exit_status = executer_a(input, env2, &locals);
 		locals.i++;
@@ -61,7 +58,7 @@ int	child_proc(t_input *input, t_exe_locals *locals, t_env2 *env2)
 	dup2(locals->fd_out, STDOUT_FILENO);
 	safe_fd_set(&locals->fd_in, STDIN_FILENO);
 	safe_fd_set(&locals->fd_out, STDOUT_FILENO);
-	safe_fd_set(&locals->fd_next, STDIN_FILENO);
+	// safe_fd_set(&locals->fd_next, STDIN_FILENO);
 	abs_cmd_path = find_exec_path(&input);
 	if (abs_cmd_path == NULL || access(abs_cmd_path, F_OK) != 0)
 	{
